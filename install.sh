f58e63a1337c09b646cbb78d7f03ab59360d101f
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-06-21 10:31:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 sudo apt install -y wget vim openjdk-8-jdk openjdk-8-jre
 
-sudo useradd --create-home jenkins
-sudo usermod --shell /bin/bash jenkins
+sudo useradd --create-home jenkinsusr
+sudo usermod --shell /bin/bash jenkinsusr
 
-sudo su - jenkins -c"wget https://updates.jenkins-ci.org/latest/jenkins.war"
+sudo su - jenkinsusr -c"wget https://updates.jenkins-ci.org/latest/jenkins.war"
 sudo cp jenkins.service /etc/systemd/system
 sudo systemctl daemon-reload
 
